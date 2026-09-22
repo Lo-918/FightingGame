5e56242210333a95442e5afa179ccc5dcc588abe
Lo-918 <lo@rindar.net>
2026-09-22 13:27:05 +0200

    Added a restart feature, critical hits and missed attacks (still needs text indicators)

--- FightingGame/Program.cs
+++ FightingGame/Program.cs
@@ -1,35 +1,93 @@
 ﻿using System.ComponentModel;
 
-int HP = 100;
-int oppHP = 100;
+int playerHP;
+int oppHP;
 int damage;
+string playerName;
+int oppNameRandomizer;
+string oppName;
+int hitRandomizer;
+string restart = "R";
 
-while (HP > 0 && oppHP > 0)
+while (restart == "R")
 {
-    damage = Random.Shared.Next(1, 11);
-    HP = HP - damage;
-    Console.Write($"PlayerA {HP}  -  ");
-    damage = Random.Shared.Next(1, 11);
-    oppHP = oppHP - damage;
-    Console.WriteLine($"{oppHP} PlayerB");
-    Console.WriteLine("");
-}
+    oppHP = 100;
+    playerHP = 100;
+    oppNameRandomizer = Random.Shared.Next(3);
 
-if (HP <= 0 && oppHP <= 0)
-{
-    Console.WriteLine("Det blev oavgjort!");
-}
-else if (HP <= 0 && oppHP >= 0)
-{
-    Console.WriteLine("Du förlorade!");
-}
-else if (HP >= 0 && oppHP <= 0)
-{
-    Console.WriteLine("Du vann!");
-}
-else
-{
-    Console.WriteLine("Ööööööh... detta är en bugg du ska inte kunna se den här texten");
-}
+    if (oppNameRandomizer == 0)
+    {
+        oppName = "The evil witch Freya";
+    }
+    else if (oppNameRandomizer == 1)
+    {
+        oppName = "The rotting zombie";
+    }
+    else
+    {
+        oppName = "The huge tarantula";
+    }
 
-Console.ReadLine();
+    Console.WriteLine("What is your name? Press enter to confirm.");
+    playerName = Console.ReadLine();
+
+    Console.WriteLine($"Get ready for the battle of the ages: {playerName} vs {oppName}");
+
+    while (playerHP > 0 && oppHP > 0)
+    {
+        Console.WriteLine("Press enter go continue.");
+        Console.ReadLine();
+
+        Console.WriteLine("~~~~~~~~~New round~~~~~~~~~");
+
+        damage = Random.Shared.Next(1, 11);
+        hitRandomizer = Random.Shared.Next(4);
+        
+        if (hitRandomizer == 0)
+        {
+            damage = 0;
+        }
+        else if (hitRandomizer == 3)
+        {
+            damage = damage * 2;
+        }
+
+        oppHP = oppHP - damage;
+        Console.WriteLine($"{playerName} deals {damage} damage to {oppName}.");
+
+        damage = Random.Shared.Next(1, 11);
+        playerHP = playerHP - damage;
+        Console.WriteLine($"{oppName} deals {damage} damage to {playerName}.");
+
+        Console.WriteLine("");
+
+        Console.Write($"{playerName} {playerHP} HP - ");
+        Console.WriteLine($"{oppHP} HP {oppName}.");
+
+        Console.WriteLine("");
+    }
+
+    if (playerHP <= 0 && oppHP <= 0)
+    {
+        Console.WriteLine("Draw!");
+    }
+    else if (playerHP <= 0 && oppHP >= 0)
+    {
+        Console.WriteLine("Game over. You lost!");
+    }
+    else if (playerHP >= 0 && oppHP <= 0)
+    {
+        Console.WriteLine("YOU WIN!");
+    }
+    else
+    {
+        Console.WriteLine("uh oh... this is very much a bug, you shouldnt be able to see this text");
+    }
+
+    Console.WriteLine("Press R to restart. Press any other key to end.");
+    restart = Console.ReadKey().KeyChar.ToString().ToUpper();
+    //Console.ReadKey läser det första tecknet man skriver (inget enter krävs)
+    //KeyChar gör om det till ett tecken
+    //ToString gör om det till en string
+    //ToUpper gör om det till versaler
+}
